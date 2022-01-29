20485ba3d12771e59d6c186bbb2539d356b71a86
Kouki Badr <badreddinekouki1998@gmail.com>
2022-01-29 18:19:08 +0100

    create main package widget

--- lib/animated_loading_button.dart
+++ lib/animated_loading_button.dart
@@ -1,7 +1,17 @@
-library animated_loading_button;
+import 'package:flutter/material.dart';
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+class AnimatedLoadingButton extends StatefulWidget {
+  const AnimatedLoadingButton({ Key? key }) : super(key: key);
+
+  @override
+  _AnimatedLoadingButtonState createState() => _AnimatedLoadingButtonState();
+}
+
+class _AnimatedLoadingButtonState extends State<AnimatedLoadingButton> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
 }

6ecde45ea6cc9c345395f2c1dd6cdb377ad1db46
Kouki Badr <badreddinekouki1998@gmail.com>
2022-02-01 21:20:18 +0100

    add inkwell widget to faded animation widget

--- lib/animated_loading_button.dart
+++ lib/animated_loading_button.dart
@@ -3,7 +3,6 @@ import 'package:animated_loading_button/animations/faded_animation.dart';
 import 'package:flutter/material.dart';
 
 class AnimatedLoadingButton<T> extends StatefulWidget {
-  
   final Future<T> Function() onPress;
   final Function(T) onAsyncCallFinished;
   final Widget child;
@@ -20,58 +19,30 @@ class AnimatedLoadingButton<T> extends StatefulWidget {
 }
 
 class _AnimatedLoadingButtonState extends State<AnimatedLoadingButton> {
-  var opacity = 1.0;
-
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: () {
-        var timer = Timer.periodic(
-          const Duration(
-            milliseconds: 1000,
-          ),
-          (timer) {
-            setState(
-              () {
-                if (opacity == 1.0) {
-                  opacity = 0;
-                } else {
-                  opacity = 1.0;
-                }
-              },
-            );
-          },
-        );
-        widget.onPress.call().then((value) {
-          timer.cancel();
-          setState(() {
-            opacity = 1;
-          });
-          widget.onAsyncCallFinished.call(value);
-        });
-      },
-      child: FadedAnimation(
-        opacity: opacity,
-        duration: const Duration(
-          milliseconds: 1000,
+    return FadedAnimation(
+      onAsyncCallFinished: widget.onAsyncCallFinished,
+      onPress: widget.onPress,
+      duration: const Duration(
+        milliseconds: 1000,
+      ),
+      child: Container(
+        width: 150,
+        height: 50,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: Colors.blue[900],
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.5),
+              spreadRadius: 2,
+              blurRadius: 2,
+              offset: const Offset(0, 1),
+            ),
+          ],
         ),
-        child: Container(
-          width: 150,
-          height: 50,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
-            color: Colors.blue[900],
-            boxShadow: [
-              BoxShadow(
-                color: Colors.grey.withOpacity(0.5),
-                spreadRadius: 2,
-                blurRadius: 2,
-                offset: const Offset(0, 1),
-              ),
-            ],
-          ),
-          child: widget.child,
-        ),
+        child: widget.child,
       ),
     );
   }
--- lib/animations/faded_animation.dart
+++ lib/animations/faded_animation.dart
@@ -1,24 +1,61 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
-class FadedAnimation extends StatelessWidget {
-
+class FadedAnimation<T> extends StatefulWidget {
   final Widget child;
-  final double opacity;
   final Duration duration;
+  final Future<T> Function() onPress;
+  final Function(T) onAsyncCallFinished;
 
   const FadedAnimation({
     Key? key,
     required this.child,
-    required this.opacity,
     required this.duration,
+    required this.onAsyncCallFinished,
+    required this.onPress,
   }) : super(key: key);
 
   @override
+  State<FadedAnimation> createState() => _FadedAnimationState();
+}
+
+class _FadedAnimationState extends State<FadedAnimation> {
+  var opacity = 1.0;
+
+  @override
   Widget build(BuildContext context) {
-    return AnimatedOpacity(
-      opacity: opacity,
-      duration: duration,
-      child: child,
+    return InkWell(
+      onTap: () {
+        var timer = Timer.periodic(
+          const Duration(
+            milliseconds: 1000,
+          ),
+          (timer) {
+            setState(
+              () {
+                if (opacity == 1.0) {
+                  opacity = 0;
+                } else {
+                  opacity = 1.0;
+                }
+              },
+            );
+          },
+        );
+        widget.onPress.call().then((value) {
+          timer.cancel();
+          setState(() {
+            opacity = 1;
+          });
+          widget.onAsyncCallFinished.call(value);
+        });
+      },
+      child: AnimatedOpacity(
+        opacity: opacity,
+        duration: widget.duration,
+        child: widget.child,
+      ),
     );
   }
 }

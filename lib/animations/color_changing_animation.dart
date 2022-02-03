7c89d8877bca3cef3b5f6bcf55ef0027f7779946
Kouki Badr <badreddinekouki1998@gmail.com>
2022-02-03 08:27:38 +0100

    add widget parameters to color changing animation widget

--- lib/animations/color_changing_animation.dart
+++ lib/animations/color_changing_animation.dart
@@ -1,17 +1,60 @@
+import 'dart:async';
+
+import 'package:animated_loading_button/widgets/button_widget.dart';
 import 'package:flutter/material.dart';
 
-class ColorChangingAnimation extends StatefulWidget {
-  const ColorChangingAnimation({ Key? key }) : super(key: key);
+class ColorChangingAnimation<T> extends StatefulWidget {
+  final Duration duration;
+  final Future<T> Function() onPress;
+  final Function(T) onAsyncCallFinished;
+  final Widget buttonChild;
+  final List<Color> colors;
+
+  const ColorChangingAnimation({
+    Key? key,
+    required this.duration,
+    required this.onPress,
+    required this.onAsyncCallFinished,
+    required this.buttonChild,
+    required this.colors,
+  }) : super(key: key);
 
   @override
   _ColorChangingAnimationState createState() => _ColorChangingAnimationState();
 }
 
 class _ColorChangingAnimationState extends State<ColorChangingAnimation> {
+  
+  late Color selectedColor;
+  late int selectedColorIndex;
+
+  @override
+  void initState() {
+    super.initState();
+    selectedColor = widget.colors[0];
+    selectedColorIndex = 0;
+  }
+  
+  
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return InkWell(
+      onTap: (){
+        Timer.periodic(widget.duration, (timer) { 
+          if(selectedColorIndex < widget.colors.length -1){
+            selectedColorIndex++;
+          }else{
+            selectedColorIndex = 0;
+          }
+          setState(() {
+            selectedColor = widget.colors[selectedColorIndex];
+          });
+        });
+      },
+      child: ButtonWidget(
+        background: selectedColor,
+        child: widget.buttonChild,
+      ),
     );
   }
 }

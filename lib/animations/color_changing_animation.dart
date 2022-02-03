0cdcbf9218bf2f02b8e9941019dd1fcb1f4ada0a
Kouki Badr <badreddinekouki1998@gmail.com>
2022-02-03 08:13:17 +0100

    add color changing animation

---
+++ lib/animations/color_changing_animation.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ColorChangingAnimation extends StatefulWidget {
+  const ColorChangingAnimation({ Key? key }) : super(key: key);
+
+  @override
+  _ColorChangingAnimationState createState() => _ColorChangingAnimationState();
+}
+
+class _ColorChangingAnimationState extends State<ColorChangingAnimation> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}

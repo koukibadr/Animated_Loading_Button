2941f53bcd395d773eec2ccce1f42f4725df5b37
Kouki Badr <badreddinekouki1998@gmail.com>
2022-02-05 09:11:36 +0100

    add text changing animation widget class

---
+++ lib/animations/text_changing_animation.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TextChangingAnimation extends StatefulWidget {
+  const TextChangingAnimation({ Key? key }) : super(key: key);
+
+  @override
+  _TextChangingAnimationState createState() => _TextChangingAnimationState();
+}
+
+class _TextChangingAnimationState extends State<TextChangingAnimation> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
